7c5d5866a50eee37c58e30ef3b746855f485c35c
Dharshini <dharshini.k2022cce@sece.ac.in>
2026-04-15 07:52:17 +0530

    Task1 - Factorial

---
+++ Readme.md
@@ -0,0 +1,5 @@
+**C# Tasks**
+**Task1:** Console - based Factorial Program.
+* Learnt about basic data types, control structures and methods in C#.
+* Implemented input validation using TryParse.
+* Used recursion to implement the factorial logic.
---
+++ Task1-Factorial/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+class Program{
+    static long Factorial(int num)
+    {
+        if(num == 0 || num == 1)
+        {
+            return 1;
+        }
+
+        return num * Factorial(num - 1);
+    }
+    static void Main(){
+        Console.WriteLine("Enter a positive Integer:");
+        int num;
+        bool isValid = int.TryParse(Console.ReadLine(), out num);
+
+        if (!isValid || num < 0)
+        {
+            Console.WriteLine("Enter a positive number");
+            return;
+        }
+        Console.WriteLine($"Factorial of {num} is {Factorial(num)}");
+
+    }
+}
