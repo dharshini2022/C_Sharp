d484ed6e20a9f261218b9169b119c999ad02009d
Dharshini <dharshini.k2022cce@sece.ac.in>
2026-04-15 08:08:52 +0530

    Task2 - Oop Exercise

--- Readme.md
+++ Readme.md
@@ -3,3 +3,8 @@
 * Learnt about basic data types, control structures and methods in C#.
 * Implemented input validation using TryParse.
 * Used recursion to implement the factorial logic.
+
+**Task2:** Simple Object - Oriented Programming Exercise
+* Implemented class and object relation
+* Implemented constructor based initiation.
+* Learnt about pillars of oops
---
+++ Task2-Oop/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Person
+{
+    public string name;
+    public int age;
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void Introduce()
+    {
+        Console.WriteLine($"Hi {name}, Welcome to C#");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Person p1 = new Person("Name1",1);
+        Person p2 = new Person("Name2",2);
+        Person p3 = new Person("Name3",3);
+
+        p1.Introduce();
+        p2.Introduce();
+        p3.Introduce();
+
+    }
+}
